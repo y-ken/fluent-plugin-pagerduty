94908d2321b8480f853d85f1447b4842b931b1cb
y-ken <y.ken.studio@gmail.com>
2013-07-15 01:17:22 +0900

    add first technical preview code

--- fluent-plugin-pagerduty.gemspec
+++ fluent-plugin-pagerduty.gemspec
@@ -1,11 +1,10 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'fluent/plugin/pagerduty/version'
 
 Gem::Specification.new do |spec|
   spec.name          = "fluent-plugin-pagerduty"
-  spec.version       = Fluent::Plugin::Pagerduty::VERSION
+  spec.version       = "0.0.1"
   spec.authors       = ["Kentaro Yoshida"]
   spec.email         = ["y.ken.studio@gmail.com"]
   spec.description   = %q{Fluentd Input plugin to call PagerDuty API.}
@@ -18,7 +17,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
   spec.add_runtime_dependency "fluentd"
   spec.add_runtime_dependency "pagerduty"
--- lib/fluent/plugin/out_pagerduty.rb
+++ lib/fluent/plugin/out_pagerduty.rb
@@ -1,9 +1,42 @@
-require "fluent/plugin/pagerduty/version"
+class Fluent::PagerdutyOutput < Fluent::Output
+  Fluent::Plugin.register_output('pagerduty', self)
 
-module Fluent
-  module Plugin
-    module Pagerduty
-      # Your code goes here...
+  def initialize
+    require 'pagerduty'
+    super
+  end
+
+  config_param :service_key, :string, :default => nil
+  config_param :event_type, :string, :default => 'trigger'
+  config_param :description, :string, :default => nil
+
+  def configure(conf)
+    super
+
+    if @service_key.nil?
+      $log.warn "pagerduty: service_key required."
+    end
+  end
+
+  def emit(tag, es, chain)
+    es.each do |time,record|
+      call_pagerduty(record)
+    end
+
+    chain.next
+  end
+
+  def call_pagerduty(record)
+    begin
+      service_key = record['service_key'] || @service_key
+      event_type = record['event_type'] || @event_type
+      description = record['description'] || record['message'] || @description
+      details = record['details'] || record
+      api = Pagerduty.new(service_key)
+      incident = api.trigger description, details
+    rescue => e
+      $log.error "pagerduty: request failed. ", :error_class=>e.class, :error=>e.message
     end
   end
 end
+
